976c98c70961fdcce5f35bfbf1043dca4d397d88
sebpers <sebastian.persson.91@gmail.com>
2025-01-08 11:31:17 +0100

    Change submit button style

--- Client/src/components/common/buttons/SubmitButtonComponent.tsx
+++ Client/src/components/common/buttons/SubmitButtonComponent.tsx
@@ -2,7 +2,7 @@ const SubmitButtonComponent = () => {
   return (
     <button
       type="submit"
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded"
       title="Submit"
     >
       Submit
