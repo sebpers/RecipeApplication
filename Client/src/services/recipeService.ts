61ed957b597d61f340846c3e867a796045ba23dc
sebpers <sebastian.persson.91@gmail.com>
2025-01-14 10:16:46 +0100

    Fix create recipe bug

--- Client/src/services/recipeService.ts
+++ Client/src/services/recipeService.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { CreateRecipeProp } from '../interfaces/CreateRecipe.ts';
-import Recipe from '../types/Recipe';
+import { CreateRecipeProp } from "../interfaces/CreateRecipe.ts";
+import Recipe from "../types/Recipe";
 
 const API_PREFIX = "http://localhost:5098/api/recipes";
 
@@ -22,44 +22,53 @@ export const getMyRecipes = async (userId: string) => {
 
     return res.data;
   } catch (err) {
-    console.error('Error retrieving users recipe:', err);
+    console.error("Error retrieving users recipe:", err);
     throw err;
   }
 };
 
 export const getRecipesListInformation = async () => {
-  const recipeList = await axios.get(`${API_PREFIX}/list-information`)
-    .then(response => {
+  const recipeList = await axios
+    .get(`${API_PREFIX}/list-information`)
+    .then((response) => {
       return response.data;
     })
-    .catch(error => {console.log(error)})
+    .catch((error) => {
+      console.log(error);
+    });
 
   return recipeList;
 };
 
 export const getById = async (id: string): Promise<Recipe> => {
-  const recipe = await axios.get(`${API_PREFIX}/${id}`)
-    .then(response => {
+  const recipe = await axios
+    .get(`${API_PREFIX}/${id}`)
+    .then((response) => {
       return response.data;
     })
-    .catch(error => {console.log(error)});
+    .catch((error) => {
+      console.log(error);
+    });
 
-  if (!recipe) { throw new Error(`Recipe with id ${id} not found.`); }
+  if (!recipe) {
+    throw new Error(`Recipe with id ${id} not found.`);
+  }
 
   return recipe;
 };
 
 export const createRecipe = async (body: CreateRecipeProp) => {
-  const createdRecipe: Recipe = await axios.post(`${API_PREFIX}`, body)
+  const createdRecipe: Recipe = await axios
+    .post(`${API_PREFIX}`, body, { withCredentials: true })
     .then((res) => {
       return res.data;
     })
-    .catch(error => {
+    .catch((error) => {
       return error.response.data;
-    })
+    });
 
   return createdRecipe;
-}
+};
 
 export const deleteRecipe = async (id: number) => {
   try {
@@ -69,7 +78,7 @@ export const deleteRecipe = async (id: number) => {
 
     return res.data;
   } catch (err) {
-    console.error('Error deleting recipe:', err);
+    console.error("Error deleting recipe:", err);
     throw err;
   }
 };
