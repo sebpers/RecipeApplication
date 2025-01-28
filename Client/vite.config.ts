5184916778938fa7f07192bf79370186b7fd265b
sebpers <sebastian.persson.91@gmail.com>
2025-01-28 10:09:53 +0100

    Add configurations

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    include: ['src/tests/**/*.test.*'],
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    include: ["src/tests/**/*.test.*"],
+  },
+  optimizeDeps: {
+    include: ["react-data-table-component"],
   },
 });
