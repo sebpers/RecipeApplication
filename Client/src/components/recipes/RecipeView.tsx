01ecb17d90761076c104fde11f6f957c618ee5c6
sebpers <sebastian.persson.91@gmail.com>
2025-02-06 12:23:04 +0100

    Add go back arrow to recipe view

--- Client/src/components/recipes/RecipeView.tsx
+++ Client/src/components/recipes/RecipeView.tsx
@@ -8,6 +8,7 @@ import RecipeCardDescription from "./card/RecipeCardDescription.tsx";
 import RecipeCardAuthorBadgeComponent from "./card/RecipeCardAuthorBadgeComponent.tsx";
 import RecipeImageComponent from "./card/RecipeImageComponent.tsx";
 import CardTitleComponent from "../common/card/CardTitleComponent.tsx";
+import { IoArrowBackSharp } from "react-icons/io5";
 
 const RecipeView = () => {
   const [recipe, setRecipe] = useState<Recipe | null>(null);
@@ -40,38 +41,46 @@ const RecipeView = () => {
     }
   };
 
-  const navigateBack = () => {
+  const navigateBack = (): void => {
     navigate(-1);
   };
 
   return (
-    <div className="border shadow-lg my-6 m:w-2/4 h-auto pb-2 mx-auto rounded-xl">
-      <RecipeImageComponent
-        recipe={recipe}
-        updateRecipe={updateRecipe}
-        updateList={navigateBack}
-        displayMenu="true"
+    <div>
+      <IoArrowBackSharp
+        onClick={() => navigateBack()}
+        title="Go back"
+        className="cursor-pointer"
       />
 
-      <div className="p-3">
-        <CardTitleComponent
-          title={recipe?.title}
-          recipeId={recipe?.id}
-          favoritedBy={recipe?.favoritedBy}
+      <div className="border shadow-lg my-6 m:w-2/4 h-auto pb-2 mx-auto rounded-xl">
+        <RecipeImageComponent
+          recipe={recipe}
+          updateRecipe={updateRecipe}
+          updateList={navigateBack}
+          displayMenu="true"
         />
 
-        <RecipeCardDescription description={recipe?.description} />
+        <div className="p-3">
+          <CardTitleComponent
+            title={recipe?.title}
+            recipeId={recipe?.id}
+            favoritedBy={recipe?.favoritedBy}
+          />
 
-        <RecipeTabs
-          ingredients={recipe?.ingredients}
-          instructions={recipe?.instructions}
-          recipeId={recipe?.id}
-        />
+          <RecipeCardDescription description={recipe?.description} />
 
-        <RecipeCardAuthorBadgeComponent
-          author={recipe?.author}
-          userId={recipe?.userId}
-        />
+          <RecipeTabs
+            ingredients={recipe?.ingredients}
+            instructions={recipe?.instructions}
+            recipeId={recipe?.id}
+          />
+
+          <RecipeCardAuthorBadgeComponent
+            author={recipe?.author}
+            userId={recipe?.userId}
+          />
+        </div>
       </div>
     </div>
   );
